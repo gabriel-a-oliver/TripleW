4b3714c688c66068e34f690974563ae298e0e00d
Gabriel O <oliver.gabriel.a@hotmail.com>
2022-12-30 18:16:28 -0800

    rebuilt project to run in rider

---
+++ TripleW/Intermediate/Build/Win64/TripleWEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.33.31630
+#include "C:/Users/olive/Documents/GitHub/TripleW/TripleW/Intermediate/Build/Win64/TripleWEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ TripleW/Source/TripleW/Private/EmptyDummy.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EmptyDummy.h"
+
+EmptyDummy::EmptyDummy()
+{
+}
+
+EmptyDummy::~EmptyDummy()
+{
+}
